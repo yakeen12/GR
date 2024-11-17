3244ed63ee2170c61b2d8b8c117e0ce19f0f1faa
yakeen12 <yakeensabha@gmail.com>
2024-11-17 21:33:21 +0300

    added login page

--- lib/main.dart
+++ lib/main.dart
@@ -1,7 +1,7 @@
 import 'package:flutter/material.dart';
 import 'package:get/get.dart';
+import 'package:music_app/Pages/login.dart';
 import 'package:music_app/homePage.dart';
-
 
 void main() {
   runApp(const MyApp());
@@ -12,27 +12,9 @@ class MyApp extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return GetMaterialApp(
+    return const GetMaterialApp(
       debugShowCheckedModeBanner: false,
-      home: const LoginPage(),
-    );
-  }
-}
-
-class LoginPage extends StatelessWidget {
-  const LoginPage({super.key});
-
-  @override
-  Widget build(BuildContext context) {
-    return Scaffold(
-      body: Center(
-          child: TextButton(
-        child: Text("Login"),
-        onPressed: () {
-          Navigator.pushReplacement(
-              context, MaterialPageRoute(builder: (context) => HomePage()));
-        },
-      )),
+      home: LoginPage(),
     );
   }
 }
