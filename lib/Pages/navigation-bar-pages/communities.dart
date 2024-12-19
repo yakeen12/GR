c5a55b79f659f1bec00c51db48272322f6fa7b00
yakeen12 <yakeensabha@gmail.com>
2024-12-19 11:37:10 +0300

    add custom widgets, cleaned the code, added playlists

---
+++ lib/Pages/navigation-bar-pages/communities.dart
@@ -0,0 +1,10 @@
+import 'package:flutter/material.dart';
+
+class Communities extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return Center(
+        child: Text('Communities',
+            style: TextStyle(fontSize: 24, color: Colors.white)));
+  }
+}
